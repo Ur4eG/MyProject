81717216388f1ce51c674f85c7fd95a3185eed1f
Ur4eG <Ur4eG@icloud.com>
2018-09-08 16:21:37 +0300

    Update ConnectionAdapter.java

--- src/home/ur4eg/dev/dds/DB/ConnectionAdapter.java
+++ src/home/ur4eg/dev/dds/DB/ConnectionAdapter.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class ConnectionAdapter {
     public static final String DATABASE = "jdbc:mysql://127.0.0.1:3306/test_db";
     public static final String USER = "root";
-    public static final String PASS = "Ebigusey9";
+    public static final String PASS = "xyz123";
     public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
     public static final String SELECT_FROM_USER = "select login, password, mail from users";
     public static final String INSERT_TO_USERS = "insert into users values (?, ?, ?)";
